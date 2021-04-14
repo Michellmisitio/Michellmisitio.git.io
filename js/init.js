2c3c216a7d0518762d437bfe8cf0150d8f2c8da4
Michellmisitio <82425009+Michellmisitio@users.noreply.github.com>
2021-04-14 00:36:54 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyATcXRRPQ3DlCSjiqk4xYBgSOmOhL9grIw",
+    authDomain: "pwa52-eb1b3.firebaseapp.com",
+    projectId: "pwa52-eb1b3",
+    storageBucket: "pwa52-eb1b3.appspot.com",
+    messagingSenderId: "737405076984",
+    appId: "1:737405076984:web:5cb4cce7c0b89ffa62420d",
+});
